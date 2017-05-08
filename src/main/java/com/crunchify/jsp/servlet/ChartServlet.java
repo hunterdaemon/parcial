3f11da87b420c072f83b582523b4a5240f884ad5
Labing <Labing@XeonB101-PC-9.academia.loc>
2017-05-08 10:33:48 -0500

    y

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -45,15 +45,13 @@ public class ChartServlet extends HttpServlet {
 	        //Crear la capa de servicios que se enlace con el DAO
                 DepartamentoDAO dep=new DepartamentoDAO();
                 Hashtable<String,Integer> c=dep.Consulta1();
-//                LinkedList keys= new LinkedList();
-//                LinkedList values= new LinkedList();
-//                keys=(LinkedList) c.keySet();
-//                values= (LinkedList) c.values();
+//                
             
                 Enumeration claves = c.keys();
+                
                 while(claves.hasMoreElements()){
                     dataset.setValue((String)claves.nextElement(),c.get((String)claves.nextElement()));
-                                     
+                     dataset.setValue((String)claves.nextElement(),c.get((String)claves.nextElement()));
 
                 }
         
