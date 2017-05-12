025e68b0e006c9131f70a44f5c7ef2ab5e057b01
Daesme <mejiaestiven110@hotmail.es>
2017-05-11 21:21:45 -0500

    grafico de barras

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -74,10 +74,10 @@ public class ChartServlet extends HttpServlet {
         List arr = new LinkedList();
        DepartamentoDAO DepartamentoDAO = new DepartamentoDAO();
         arr =   DepartamentoDAO.Consulta1();
-        double[][] data = new double[1][arr.size()-2];
+        double[][] data = new double[1][arr.size()/2];
         for (int i = 0; i < arr.size(); i=i+2) {
             
-            data[0][i] = (Integer)arr.get(i+1) ;
+            data[0][i+1] = (Integer)arr.get(i+1) ;
         }
 
         CategoryDataset category = DatasetUtilities.createCategoryDataset(
